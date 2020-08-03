b91845a2c0a87210cffbd87f87b58820487e9f6b
Dazikiri Anikar <mierzwinski.b@gmail.com>
2020-08-03 20:31:48 +0200

    Module II. Added unit tests with AssertJ.

--- src/main/java/bm/app/moduleTwo/VatService.java
+++ src/main/java/bm/app/moduleTwo/VatService.java
@@ -11,14 +11,14 @@ public class VatService {
         this.vatValue = new BigDecimal("0.23");
     }
 
-    public BigDecimal getGrossPriceForDefaultVat(Product product) {
+    public BigDecimal getGrossPriceForDefaultVat(Product product) throws IncorrectVatException {
         return getGrossPrice(product.getNetPrice(), vatValue);
     }
 
-    public BigDecimal getGrossPrice(BigDecimal netPrice, BigDecimal vatValue) {
+    public BigDecimal getGrossPrice(BigDecimal netPrice, BigDecimal vatValue) throws IncorrectVatException {
         MathContext precision = new MathContext(4);
         if (vatValue.compareTo(BigDecimal.ONE) == 1) {
-            throw new
+            throw new IncorrectVatException("VAT cannot be higher than 1.");
         }
         return netPrice.multiply(vatValue.add(BigDecimal.ONE)).round(precision);
     }
--- src/test/java/bm/app/moduleTwo/VatServiceTest.java
+++ src/test/java/bm/app/moduleTwo/VatServiceTest.java
@@ -7,9 +7,8 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class VatServiceTest {
 
@@ -17,25 +16,34 @@ class VatServiceTest {
 
     @Test
     @DisplayName("Should calculate the gross price including a default VAT value for a given product.")
-    void shouldCalculateGrossPriceForDefaultVat(){
+    void shouldCalculateGrossPriceForDefaultVat() throws IncorrectVatException {
         Product product = generateAProductWithThePrice("100.0");
         assertThat(vatService.getGrossPriceForDefaultVat(product)).isEqualTo("123.0");
     }
 
     @Test
     @DisplayName("Should calculate the gross price including a provided VAT value for a given product.")
-    void shouldCalculateGrossPriceForGivenVatValue(){
+    void shouldCalculateGrossPriceForGivenVatValue() throws IncorrectVatException {
         Product product = generateAProductWithThePrice("10.0");
         BigDecimal providedVat = new BigDecimal("0.07");
         assertThat(vatService.getGrossPrice(product.getNetPrice(), providedVat)).isEqualTo("10.70");
     }
 
-    private Product generateAProductWithThePrice(String price){
+    @Test
+    @DisplayName("Should throw an IncorrectVatException class when provided VAT value is higher than 1.")
+    void shouldThrowIncorrectVatExceptionWhenVatValueIsTooHigh() {
+        Product product = generateAProductWithThePrice("120.0");
+        assertThrows(IncorrectVatException.class, () -> {
+            vatService.getGrossPrice(product.getNetPrice(), BigDecimal.TEN);
+        });
+    }
+
+    private Product generateAProductWithThePrice(String price) {
         return new Product(UUID.randomUUID(), new BigDecimal(price));
     }
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         vatService = new VatService();
     }
 
