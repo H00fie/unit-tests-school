c84db8749da3e647b1b46fe8cddabf3b603bc828
Dazikiri Anikar <mierzwinski.b@gmail.com>
2020-08-04 23:42:07 +0200

    NameInverterKata. Added a test for an empty String.

--- src/main/java/bm/app/nameInverterKata/NameInverter.java
+++ src/main/java/bm/app/nameInverterKata/NameInverter.java
@@ -3,6 +3,9 @@ package bm.app.nameInverterKata;
 public class NameInverter {
 
     public String invert(String name){
+        if (name.isEmpty()){
+            return "";
+        }
         throw new NullPointerException();
     }
 
--- src/test/java/bm/app/nameInverterKata/NameInverterTest.java
+++ src/test/java/bm/app/nameInverterKata/NameInverterTest.java
@@ -1,18 +1,31 @@
 package bm.app.nameInverterKata;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class NameInverterTest {
 
-    NameInverter inverter = new NameInverter();
+    private NameInverter inverter;
 
     @Test
     void shouldThrowExceptionWhenNullProvided(){
         assertThatExceptionOfType(NullPointerException.class).isThrownBy(() -> {
            inverter.invert(null);
         });
+    }
+
+    @Test
+    void shouldReturnAnEmptyStringWhenAnEmptyStringIsProvided(){
+        String inverted = inverter.invert("");
+        assertThat(inverted).isEqualTo("");
+    }
+
+    @BeforeEach
+    void setUp(){
+        inverter = new NameInverter();
     }
 
 }
