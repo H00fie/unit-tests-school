c89fc1951cb6e5027b15fda6c6e95dc8af076798
Bartosz76 <mierzwinski.b@gmail.com>
2022-01-07 13:51:35 +0100

    Module III. Refactored the initial tests.

--- src/test/java/bm/app/moduleThree/BowlingKataTest.java
+++ src/test/java/bm/app/moduleThree/BowlingKataTest.java
@@ -1,9 +1,8 @@
 package bm.app.moduleThree;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,14 +11,46 @@ class BowlingKataTest {
     BowlingKata bowlingKata;
 
     @Test
+    @DisplayName("No throws made yet, so the score is zero.")
     void shouldScoreOneWhenNoRolls() {
         assertThat(bowlingKata.getScore()).isEqualTo(0);
     }
 
     @Test
+    @DisplayName("A roll fells one pin, so the score is one.")
     void shouldScoreOneWhenRolledOne() {
         bowlingKata.roll(1);
         assertThat(bowlingKata.getScore()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("20 throws are made and 1 pin is felled every time.")
+    void shouldScoreTwentyWhenOnePinDownTwentyTimes() {
+        //given
+        int numberOfThrows = 20;
+        int pinsFell = 1;
+        //when
+        rolling(numberOfThrows, pinsFell);
+        //then
+        assertThat(bowlingKata.getScore()).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("20 throws are made and none of them fells a pin.")
+    void shouldScoreZeroWhenNoPinDownTwentyTimes() {
+        //given
+        int numberOfThrows = 20;
+        int pinsFell = 0;
+        //when
+        rolling(numberOfThrows, pinsFell);
+        //then
+        assertThat(bowlingKata.getScore()).isEqualTo(0);
+    }
+
+    private void rolling(int numberOfThrows, int pins) {
+        for (int i = 0; i < numberOfThrows; i++) {
+            bowlingKata.roll(pins);
+        }
     }
 
     @BeforeEach
